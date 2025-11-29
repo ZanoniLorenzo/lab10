6e8c279660981af11ece54d36342dd2b9843ea01
Lorenzo Zanoni <lorenzo.zanoni3@studio.unibo.it>
2025-11-29 14:46:37 +0100

    ex 02 done, missing double view

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,18 +1,57 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    //private static final int MIN = 0;
+    //private static final int MAX = 100;
+    //private static final int ATTEMPTS = 10;
+    private int min, max, attempts;
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
+
+    private void inputFile(){
+        final File input = new File("src/main/resources/config.yml");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(input), StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e1) {
+            throw new IllegalStateException("Config file not found");
+        }
+        String line = "";
+        int index;
+        for (int i = 0; i < 3; i++){
+            try {
+                line = reader.readLine();
+                index = line.indexOf(" ") + 1;
+                if (i == 0) {
+                    min = Integer.parseInt(line.substring(index));
+                } else if (i == 1) {
+                    max = Integer.parseInt(line.substring(index));
+                } else if (i == 2) {
+                    attempts = Integer.parseInt(line.substring(index));
+                }
+            } catch (IOException e) {
+            System.out.println("reading error"); // NOPMD
+            }
+        }
+        try {
+                reader.close();
+        } catch (IOException e){
+            System.out.println("closing error"); //NOPMD
+        }
+    }
 
     /**
      * @param views
@@ -27,7 +66,9 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        inputFile();
+        this.model = new DrawNumberImpl(min, max, attempts);
+        assert model != null;
     }
 
     @Override
